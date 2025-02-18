b70af34eb86152c06658f043eff59984b1d7830f
Saaifa <Contact2saaifa@gmail.com>
2025-02-18 16:15:52 +0530

    third commit

--- lib/core/api/api_constants.dart
+++ lib/core/api/api_constants.dart
@@ -4,6 +4,7 @@ class ApiConstants{
   static const String GOOGLE_BASEURL = "https://maps.googleapis.com/maps/api/place/";
   static const String WEATHER_BASEURL = "https://api.openweathermap.org/data/2.5/weather";
 
+
   static const String GOOGLE_API_KEY = 'YOUR_API_KEY';
   static const String WEATHER_API_KEY = '86c4884a76c4e8202ebc53f968594588';
 
