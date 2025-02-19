679eb5e81c7883acf4b718d4e23d0ab19b68f708
Saaifa <87118607+Saaifa@users.noreply.github.com>
2025-02-19 11:16:04 +0530

    Update weather_api_service.dart

--- lib/data/source/weather_api_service.dart
+++ lib/data/source/weather_api_service.dart
@@ -1,18 +1,3 @@
-// import 'package:dio/dio.dart';
-// import 'package:flutter_weather_app/core/api/api_constants.dart';
-// import 'package:flutter_weather_app/data/model/weather_model.dart';
-// import 'package:retrofit/http.dart';
-//
-// @RestApi(baseUrl: WeatherApi.WEATHER_BASEURL)
-//
-// abstract class WeatherApiService{
-//   factory WeatherApiService(Dio dio, {String baseUrl}) = _WeatherApiService;
-//
-//   @GET("")
-//   Future<WeatherModel> getWeatherData(
-//       @Query("") int
-//       );
-// }
 
 import 'package:dio/dio.dart';
 import 'package:flutter_weather_app/core/api/api_constants.dart';
@@ -23,7 +8,7 @@ class WeatherApiService{
   final String _apiKey = ApiConstants.WEATHER_API_KEY;
 
   Future<Map<String, dynamic>> getWeatherData(String latitude, String longitude) async {
-    final response =  await _dio.get("$_baseUrl?lat=23.2419997&lon=69.66693239999999&appid=86c4884a76c4e8202ebc53f968594588");
+    final response =  await _dio.get("$_baseUrl?lat=23.2419997&lon=69.66693239999999&appid="API_KEY");
     return response.data;
   }
 }
